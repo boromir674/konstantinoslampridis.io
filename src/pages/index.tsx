fca9275b7c8b41f330cc1f37b0a4b09ef9e738e6
konstantinos <boromir674@hotmail.com>
2023-07-06 15:03:22 +0300

    build <title> element with user data (GraphQL)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,43 @@
 import React from "react";
 import type { HeadFC } from "gatsby";
-import IndexPage from '../Components/App';
+import IndexPage from "../Components/App";
+import BuildTimeTitle from '../Components/TitleElement';
 
-
+// BODY element
+// DECLARE what element tree to put in the <body> of html (index) page
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+// TITLE element
+
+// since we use gatsby as static site generator
+// we set the <title> html element text to the user name at build time assuming
+// it is extracted in the GraphQL layer
+
+
+
+// type BuildTimeSourcePluginDataGraphQLData = {
+//   userDefinedWebsiteData: {
+//     personal: {
+//       name: string;
+//     };
+//   };
+// };
+
+
+// const {
+//   userDefinedWebsiteData: {
+//     personal: { name },
+//   },
+// } = useStaticQuery<BuildTimeSourcePluginDataGraphQLData>(graphql`
+//   query {
+//     userDefinedWebsiteData {
+//       personal {
+//         name
+//       }
+//     }
+//   }
+// `);
+
+// DECLARE the 'content' of the <title> element of the html (index) page
+export const Head: HeadFC = () => <BuildTimeTitle/>;
+// export const Head: HeadFC = () => <title>{name}</title>;

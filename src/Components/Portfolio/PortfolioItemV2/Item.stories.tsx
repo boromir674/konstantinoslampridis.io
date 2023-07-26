cd6affca60caae905d25ae7f8669c60a8cae1f95
konstantinos <boromir674@hotmail.com>
2023-07-26 20:47:24 +0300

    feat: update Portfolio/PortfolioItemV2/Item and Stories

--- src/Components/Portfolio/PortfolioItemV2/Item.stories.tsx
+++ src/Components/Portfolio/PortfolioItemV2/Item.stories.tsx
@@ -10,5 +10,18 @@ export default {
 export const Default = {
     args: {
       // same interface as the props of the Component
+      data: {
+        title: "Python Package Generator",
+        development_period: "2020-2021",
+        status: "Mature",
+        description: "A CLI tool to generate a Python package with a single command.",
+        source_code_repo: "boromir674/cookiecutter-python-package",
+        release: [{
+          artifact_type: "pypi",
+          version: "0.1.0",
+          name: "cookiecutter-python-package",
+        }],
+        tags: ["Python", "CLI", "Automation", "Docker"],
+      },
     },
 };
--- src/Components/Portfolio/PortfolioItemV2/Item.tsx
+++ src/Components/Portfolio/PortfolioItemV2/Item.tsx
@@ -1,90 +1,91 @@
+import { FC } from "react";
 import styled from "@emotion/styled";
+
+import PortfolioItemInterface from "../../../PortfolioItemInterface";
 
 const FrameContainer = styled.div`
   background-color: #ffffff;
   display: flex;
   flex-direction: row;
   justify-content: center;
-  width: 100%;
+  // width: 100%;
+  // set width to be as inner elemenet
+  // width: fit-content;
+  border: 1px solid;
 `;
-
-// width: 100%;
-// height: 100vh;
-// background-color: #f0f0f0;
-// padding: 20px;
-// box-sizing: border-box;
 
 // App Portfolio Item Card - Outer most Container
 const AppPortfolioItemCard = styled.div`
-
   background-color: #ffffff;
+  display: flex;
   height: 293px;
   position: relative;
   width: 574px;
+`;
+
+const TopPartOfCard = styled.div`
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
   border: 1px solid;
 `;
 
-// Releases Pane
-const AppReleasesPane = styled.div`
+// PROJECT TITLE
+const TitleContainer = styled.div`
+  position: absolute;
+  //   color: #000000;
+  //   font-family: "Kokoro-Regular", Helvetica;
+  //   font-size: 18px;
+  //   font-weight: 400;
+  //   left: 18px;
+  //   letter-spacing: 0;
+  //   line-height: normal;
+  //   position: absolute;
+  //   top: 17px;
+  //   width: 212px;
+  box-sizing: border-box;
+  // content or box type
+  display: flex;
+  flex-wrap: wrap;
+  // wrap text below if container gets smaller
+  height: 40px;
+  // height of container
+  left: 18px;
+  // left margin
+  // position relative to parent container
+  top: 17px;
+  // top margin
+`;
+// PROJECT TITLE SPAN
+const TitleSpan = styled.span`
+  padding: 0px 10px;
+  font-size: 18px;
+  font-weight: 400;
+  font-family: "Kokoro-Regular", Helvetica;
+  color: #000000;
+  letter-spacing: 0;
+  line-height: normal;
+  text-align: left;
+  top: 47px;
+`;
 
+// Right Pane with Maturity Level and Releases
+const AppReleasesPane = styled.div`
   border: 1px solid;
   background-color: #00000033;
   box-shadow: 0px 4px 4px #00000040;
-  height: 140px;
+  height: 110px;
+  // height: max-content;
   left: 354px;
   position: absolute;
   top: 18px;
-  width: 202px;
-`;
-
-// PYPI Release Container
-const PyPIReleaseContainer = styled.div`
-  height: 23px;
-  left: 18px;
-  width: fit-content;
-  position: absolute;
-  top: 67px;
-  // set width to automatically fit content and also not surpass a max value
-  display: flex;
-flex-wrap: wrap;
-`;
-
-// PYPI Release Text Container
-const TextWrapper = styled.div`
-  color: #000000;
-  font-family: "Kokoro-Regular", Helvetica;
-  font-size: 15px;
-  font-weight: 400;
-  left: 9px;
-  letter-spacing: 0;
-  line-height: normal;
-  position: absolute;
-  top: 2px;
-  width: 133px;
-`;
-
-// DOCKER Release Container
-const DockerReleaseContainer = styled.div`
-  height: 20px;
-  left: 26px;
-  position: absolute;
-  top: 100px;
-  width: 124px;
-`;
-
-// Docker Release Text Container
-const TextWrapper2 = styled.div`
-  color: #000000;
-  font-family: "Kokoro-Regular", Helvetica;
-  font-size: 15px;
-  font-weight: 400;
-  left: 5px;
-  letter-spacing: 0;
-  line-height: normal;
-  position: absolute;
-  top: 1px;
-  white-space: nowrap;
-  width: 113px;
+  //   width: 202px;
+  width: auto;
+  // display: flex;
+  // flex-wrap: wrap; /* Wrap inner elements below */
+  // align-content: flex-start; /* Align items to the top */
 `;
 
 // Software Maturity Level Container (ie stable, mature, beta, alpha, etc)
@@ -92,12 +93,12 @@ const SoftwareMaturityContainer = styled.div`
   height: 40px;
   left: 6px;
   position: absolute;
-  top: 8px;
+  top: 2px;
   width: 186px;
 `;
 
 // Software Maturity Level Text Container
-const SoftwareMaturity = styled.div`
+const SoftwareMaturity = styled.span`
   color: #000000;
   font-family: "Kokoro-Regular", Helvetica;
   font-size: 15px;
@@ -109,52 +110,88 @@ const SoftwareMaturity = styled.div`
   top: 7px;
 `;
 
-// PROJECT TITLE
-const TitleContainer = styled.div`
-//   color: #000000;
-//   font-family: "Kokoro-Regular", Helvetica;
-//   font-size: 18px;
-//   font-weight: 400;
-//   left: 18px;
-//   letter-spacing: 0;
-//   line-height: normal;
-//   position: absolute;
-//   top: 17px;
-//   width: 212px;
-box-sizing: border-box;
-// content or box type
-display: flex;
-flex-wrap: wrap;
-// wrap text below if container gets smaller
-height: 40px;
-// height of container
-left: 18px;
-// left margin
-position: absolute;
-// position relative to parent container
-top: 17px;
-// top margin
+// RELEASES PANE in PyPI, Docker, etc
+const ReleasesPane = styled.div`
+  position: absolute;
+  top: -20px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  height: 100%;
+  width: 100%;
+`;
 
+// PYPI Release Container
+const PyPIReleaseContainer = styled.div`
+  height: 23px;
+  left: 18px;
+  // width: fit-content;
+  position: absolute;
+  top: 67px;
+  inline-size: min-content;
+  // display: inline-block;
+  //   display: flex;
+  //   flex-direction: column;
+  //   flex-wrap: wrap; /* Wrap inner elements below */
 `;
-// PROJECT TITLE SPAN
-const TitleSpan = styled.span`
-    padding: 0px 10px;
-    font-size: 18px;
-    font-weight: 400;
-    font-family: "Kokoro-Regular", Helvetica;
-    color: #000000;
-    letter-spacing: 0;
-    line-height: normal;
-    text-align: left;
-    top: 47px;
+
+// PYPI Release Text Container
+const TextWrapper = styled.span`
+  // display: flex;
+  // flex-wrap: wrap; /* Wrap inner elements below */
+  color: #000000;
+  font-family: "Kokoro-Regular", Helvetica;
+  font-size: 15px;
+  font-weight: 400;
+  left: 9px;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  top: 2px;
+  // width: fit-content;
+  // width: 100%;
+  high: auto;
+  width: 113px;
+  overflow-wrap: break-word;
 `;
+
+// DOCKER Release Container
+const DockerReleaseContainer = styled.div`
+  height: 20px;
+  left: 18px;
+  position: absolute;
+  top: 100px;
+  width: 124px;
+`;
+
+// Docker Release Text Container
+const TextWrapper2 = styled.div`
+  color: #000000;
+  font-family: "Kokoro-Regular", Helvetica;
+  font-size: 15px;
+  font-weight: 400;
+  left: 9px;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  top: 1px;
+  white-space: nowrap;
+  width: 113px;
+`;
+
 // POINTERS Container (ie links to source code, CI, etc)
 const PointersContainer = styled.div`
   height: 102px;
   left: 22px;
   position: absolute;
-  top: 56px;
+  top: 36px;
   width: 259px;
+  box-sizing: border-box;
+  // content or box type
+  display: flex;
+  flex-wrap: wrap;
+  // wrap text below if container gets smaller
 `;
 
 // Source Code Pointer Container
@@ -164,10 +201,16 @@ const SourceCodeContainer = styled.div`
   position: absolute;
   top: 13px;
   width: 211px;
+  box-sizing: border-box;
+  // content or box type
+  // display: inline-block;
+  // flex-wrap: wrap;
+  // wrap text below if container gets smaller
 `;
 
 // Source Code Link/Pointer Content
 const P = styled.p`
+  display: flex;
   color: #000000;
   font-family: "Kokoro-Regular", Helvetica;
   font-size: 15px;
@@ -191,11 +234,13 @@ const CIContainer = styled.div`
 
 // CI Link/Pointer/URL Content
 const CIContent = styled.div`
+  display: flex;
+  flex-wrap: wrap;
   color: #000000;
   font-family: "Kokoro-Regular", Helvetica;
   font-size: 15px;
   font-weight: 400;
-  left: 9px;
+  left: 7px;
   letter-spacing: 0;
   line-height: normal;
   position: absolute;
@@ -225,40 +270,56 @@ const ProjectDescriptionContent = styled.p`
   top: 7px;
   width: 448px;
 `;
+interface PortfolioItemProps {
+  data: PortfolioItemInterface;
+}
 
-const Frame = () => {
+const Frame: FC<PortfolioItemProps> = ({
+  data: {
+    title,
+    development_period,
+    status,
+    description,
+    source_code_repo,
+    release,
+    tags,
+  },
+}) => {
   return (
     <FrameContainer>
       <AppPortfolioItemCard>
-        {/* RELEASES PANE */}
-        <AppReleasesPane>
-          <PyPIReleaseContainer>
-            <TextWrapper>PYPI Release</TextWrapper>
-          </PyPIReleaseContainer>
-          <DockerReleaseContainer>
-            <TextWrapper2>Docker Release</TextWrapper2>
-          </DockerReleaseContainer>
-          <SoftwareMaturityContainer>
-            <SoftwareMaturity>Software Maturity Level</SoftwareMaturity>
-          </SoftwareMaturityContainer>
-        </AppReleasesPane>
-        {/* TITLE */}
-        <TitleContainer><TitleSpan>Python Package Generator</TitleSpan></TitleContainer>
-        <PointersContainer>
-          <SourceCodeContainer>
-            <P>Logo + Source Code URL</P>
-          </SourceCodeContainer>
-          <CIContainer>
+        <TopPartOfCard>
+          {/* TITLE */}
+          <TitleContainer>
+            <TitleSpan>{title}</TitleSpan>
+          </TitleContainer>
+          {/* RELEASES PANE */}
+          <AppReleasesPane>
+            <SoftwareMaturityContainer>
+              <SoftwareMaturity>Dev stage: {status}</SoftwareMaturity>
+            </SoftwareMaturityContainer>
+            <ReleasesPane>
+              <PyPIReleaseContainer>
+                <TextWrapper>PYPI Release</TextWrapper>
+              </PyPIReleaseContainer>
+              <DockerReleaseContainer>
+                <TextWrapper2>Docker Release</TextWrapper2>
+              </DockerReleaseContainer>
+            </ReleasesPane>
+          </AppReleasesPane>
+          {/* POINTERS PANE */}
+          <PointersContainer>
+            <SourceCodeContainer>
+              <P>Github: {source_code_repo}</P>
+            </SourceCodeContainer>
+            {/* <CIContainer>
             <CIContent>Logo + CI URL</CIContent>
-          </CIContainer>
-        </PointersContainer>
+          </CIContainer> */}
+          </PointersContainer>
+        </TopPartOfCard>
+        {/* PROJECT DESCRIPTION */}
         <ProjectDescriptionContainer>
-          <ProjectDescriptionContent>
-            A Python Package hosting multiple (CLI) programs that facilitate
-            Topic Modeling research operations, on a
-            <br />
-            collection of documents.
-          </ProjectDescriptionContent>
+          <ProjectDescriptionContent>{description}</ProjectDescriptionContent>
         </ProjectDescriptionContainer>
       </AppPortfolioItemCard>
     </FrameContainer>

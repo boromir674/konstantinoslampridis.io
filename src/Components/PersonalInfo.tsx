435783df51e71b6f17838de558c5fa386b05659f
konstantinos <boromir674@hotmail.com>
2023-07-30 17:39:34 +0300

    feat: enhance Personal Info/Contact Section with links to 'Resume' & 'CV + Projects' online PDF docs

--- src/Components/PersonalInfo.tsx
+++ src/Components/PersonalInfo.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import styled from "@emotion/styled";
+// import "@fontawesome";
+// import { faFilePdf, faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+
+// Open PDF in new TAB ICON
+import AppExternalURLIcon from "./AppExternalURLIcon";
+
 import PersonalInfoInterface from "../PersonalInfoInterface";
 
 interface ContainerProps {
@@ -51,6 +57,21 @@ const InfoValue = styled.span`
   margin-top: 5px;
 `;
 
+const InfoURLVerticalFlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 10px;
+`;
+
+const InfoURLVerticalFlexContainerItem = styled.div<URLProps>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 5px;
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Link = styled.a<URLProps>`
   color: ${(props) => props.theme.textColor};
   // color: #2063e9;
@@ -66,14 +87,19 @@ const MyLink = styled(Link)`
   margin-right: 10px;
 `;
 
-// const GitLabLink = styled(Link)`
-//   margin-right: 10px;
-// `;
+// Open PDF in new TAB ICON
+// library.add(faB, faS, faHouseLaptop, faCheckSquare, faCoffee, faDatabase, faWindowMaximize)
+const IconWrapper = styled.span`
+  margin-right: 8px;
+`;
+const ExternalLinkIcon = () => (
+  <IconWrapper>
+    <i className="fas fa-external-link-alt"></i>
+  </IconWrapper>
+);
 
-// const LinkedInLink = styled(Link)`
-//   margin-right: 10px;
-// `;
 
+// TODO read ALL Data from props
 const PersonalInfo: React.FC<PersonalInfoProps> = ({
   userData: { name, email, github, gitlab, linkedin },
   theme,
@@ -97,7 +123,7 @@ const PersonalInfo: React.FC<PersonalInfoProps> = ({
               textColor: theme.linkColor,
             }}
           >
-            {github}
+            {github} <AppExternalURLIcon />
           </MyLink>
         </InfoValue>
       </InfoItem>
@@ -112,7 +138,7 @@ const PersonalInfo: React.FC<PersonalInfoProps> = ({
               textColor: theme.linkColor,
             }}
           >
-            {gitlab}
+            {gitlab} <AppExternalURLIcon />
           </MyLink>
         </InfoValue>
       </InfoItem>
@@ -127,25 +153,42 @@ const PersonalInfo: React.FC<PersonalInfoProps> = ({
               textColor: theme.linkColor,
             }}
           >
-            {linkedin}
+            {linkedin} <AppExternalURLIcon />
           </MyLink>
         </InfoValue>
       </InfoItem>
       {/* Render the various PDF documents and link to their publicly accessible urls */}
       <InfoItem>
-        <InfoKey>Resume:</InfoKey>{" "}
-        <InfoValue>
-          <Link
-            href={`https://konstantinos-lampridis-cv-documents.s3.eu-central-1.amazonaws.com/main_cv%2Bprojects.pdf`}
-            target="_blank"
-            rel="noopener noreferrer"
-            theme={{
-              textColor: theme.linkColor,
-            }}
-          >
-            CV + Projects PDF
-          </Link>
-        </InfoValue>
+        {" "}
+        <InfoKey>Curricular Docs:</InfoKey>{" "}
+        <InfoURLVerticalFlexContainer>
+          {/* 1 PAGE RESUME */}
+          <InfoURLVerticalFlexContainerItem theme={{ textColor: "#fff" }}>
+            <MyLink
+              href={`https://konstantinos-lampridis-cv-documents.s3.eu-central-1.amazonaws.com/main_resume.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+              theme={{
+                textColor: theme.linkColor,
+              }}
+            >
+              '1 Page Resume' <AppExternalURLIcon />
+            </MyLink>
+          </InfoURLVerticalFlexContainerItem>
+          {/* CV + PROJECTS */}
+          <InfoURLVerticalFlexContainerItem theme={{ textColor: "#fff" }}>
+            <MyLink
+              href={`https://konstantinos-lampridis-cv-documents.s3.eu-central-1.amazonaws.com/main_cv%2Bprojects.pdf`}
+              target="_blank"
+              rel="noopener noreferrer"
+              theme={{
+                textColor: theme.linkColor,
+              }}
+            >
+              'CV + Projects' <AppExternalURLIcon />
+            </MyLink>
+          </InfoURLVerticalFlexContainerItem>
+        </InfoURLVerticalFlexContainer>
       </InfoItem>
     </Container>
   );

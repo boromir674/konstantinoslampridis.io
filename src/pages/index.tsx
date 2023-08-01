a0a9fe9ab58a053a08d5a29ba058585d9c5c37f0
konstantinos <boromir674@hotmail.com>
2023-08-01 03:08:06 +0300

    remove font-awesome dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,6 @@ import BuildTimeTitle from "../Components/TitleElement";
 
 import IndexPage from "../Components/App2";
 
-//////// FONT AWESOME configuration ////////
-
-// See https://stackoverflow.com/questions/56334381/why-my-font-awesome-icons-are-being-displayed-big-at-first-and-then-updated-to-t
-// See https://github.com/FortAwesome/react-fontawesome/issues/134
-
-// The following import prevents a Font Awesome icon server-side rendering bug,
-// where the icons flash from a very large icon down to a properly sized one:
-import '@fortawesome/fontawesome-svg-core/styles.css';
-
-// Prevent fontawesome from adding its CSS since we did it manually above:
-import { config } from '@fortawesome/fontawesome-svg-core';
-
-config.autoAddCss = false; /* eslint-disable import/first */
-
 
 //////// BODY <body> element ////////
 

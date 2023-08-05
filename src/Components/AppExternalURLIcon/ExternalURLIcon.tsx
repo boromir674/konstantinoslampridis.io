0504a6577392039a420528a44d30139bd0bee8d2
konstantinos <boromir674@hotmail.com>
2023-08-05 22:15:55 +0300

    design: increase line width of svg url-link icon

--- src/Components/AppExternalURLIcon/ExternalURLIcon.tsx
+++ src/Components/AppExternalURLIcon/ExternalURLIcon.tsx
@@ -9,27 +9,18 @@ interface AppExternalURLIconProps {
 const AppExternalURLIcon: FC<AppExternalURLIconProps> = ({
   theme: { lineColor },
 }) => {
-  console.log("LINE COLOR: ", lineColor);
   return (
     <svg
       width="10px"
       height="10px"
       viewBox="-2.56 -2.56 69.12 69.12"
       // governs line width
-      stroke-width="3.108"
+      strokeWidth="5.108"
       // governs color of the icon
       stroke={lineColor}
       // stroke="blue"
       fill="none"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0" />
-
-      <g
-        id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-
       <g id="SVGRepo_iconCarrier">
         <path d="M55.4,32V53.58a1.81,1.81,0,0,1-1.82,1.82H10.42A1.81,1.81,0,0,1,8.6,53.58V10.42A1.81,1.81,0,0,1,10.42,8.6H32" />
 

41835fa9d892223a3c454d0442d2d491520a0d0f
konstantinos <boromir674@hotmail.com>
2023-08-20 12:42:13 +0300

    improve docs

--- src/js-yaml.d.ts
+++ src/js-yaml.d.ts
@@ -1,3 +1,9 @@
+/* we provide the type definition for js-yaml, since @types/js-yaml does not seem to work */
+
+// example attempt to add module tye declarations:
+// yarn add --dev @types/js-yaml
+
+// each module declaration should have a 'declare module'
 declare module "js-yaml" {
   type YamlClient = {
     safeLoad: (yamlData: string) => any;

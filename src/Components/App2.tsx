596957e9542bd993bf8093fd72f505f2bfa2c72f
konstantinos <boromir674@hotmail.com>
2024-06-18 22:00:09 +0300

    refactor: adjust App Component to ancestor component(s) changes

--- src/Components/App2.tsx
+++ src/Components/App2.tsx
@@ -1,9 +1,15 @@
 import { graphql, useStaticQuery } from "gatsby";
-import type { FC } from "react";
+import React, { FC } from "react";
+
 import BigScreenViewInteractive from "./BigScreenViewInteractive";
 import { mergeStylings, commonStyling, ComputedTheme } from "../AppStyles";
 import lightMode from '../LightMode';
 import darkMode from '../DarkMode';
+import { UserDefinedTextData } from '../types';
+
+// Leverage CSS modules to do CSS reset
+// import '../global.css'; // Import the global CSS reset
+
 
 interface EducationItemUserTextData {
   name: string;
@@ -14,10 +20,14 @@ interface EducationItemUserTextData {
   topics: string[];
 }
 
+interface BuildTimeData {
+  userDefinedWebsiteData: UserDefinedTextData
+}
+lightMode.portfolio.item.resourceLinks
 const App: FC = () => {
   const {
     userDefinedWebsiteData: { personal, education, professional, portfolio },
-  } = useStaticQuery(graphql`
+  } = useStaticQuery<BuildTimeData>(graphql`
     query {
       userDefinedWebsiteData {
         personal {
@@ -82,142 +92,206 @@ const App: FC = () => {
   const lightTheme: ComputedTheme = mergeStylings(lightMode, commonStyling);
   const darkTheme: ComputedTheme = mergeStylings(darkMode, commonStyling);
 
+  // compute maximum number of Releases contained in a single portfolio item
+  const maxNumberOfReleasesPerPortfolioItems = portfolio.reduce(
+    (acc, { release = [] }) => Math.max(acc, release.length),
+    0
+  );
+  // compute maximum number of links contained in a single portfolio item
+  const maxNumberOfLinksPerPortfolioItems = portfolio.reduce(
+    (acc, { resource_links = [] }) => Math.max(acc, resource_links.length),
+    0
+  );
 
   return (
     // <main>
-    <BigScreenViewInteractive
-      navigationSections={[
-        {
-          htmlID: "introduction-section",
-          barLabel: "Introduction",
-        },
-        {
-          htmlID: "professional-section",
-          barLabel: "Professional",
-        },
-        {
-          htmlID: "open-source-portfolio-section",
-          barLabel: "Open Source Portfolio",
-        },
-      ]}
-      data={{
-        verticalMainPane: {
-          introduction: {
-            name: personal.name,
+      <BigScreenViewInteractive
+        // style={{}}
+        navigationSections={[
+          {
+            htmlID: "introduction-section",
+            barLabel: "Introduction",
           },
-          professional: professional.experience_items,
-          portfolio,
-        },
-
-        verticalSidePane: {
-          personal: {
-            name: personal.name,
-            email: personal.email,
-            github: name2Url["github"],
-            gitlab: name2Url["gitlab"],
-            linkedin: name2Url["linkedin"],
+          {
+            htmlID: "professional-section",
+            barLabel: "Professional",
           },
-          education: education.map(
-            (item: EducationItemUserTextData, index: number) => ({
-              degree_title: item.degree,
-              university_name: item.name,
-              location: item.location,
-              duration: item.date,
-              thesis_title: item.thesis_title,
-              topics: item.topics,
-            })
-          ),
-        },
-      }}
-      colorSet={{
-        // LIGHT THEME - MODE
-        light: {
-          containerBackgroundColor: lightTheme.backgroundColor,
-          topHeaderPane: {
-            themeSwitch: lightTheme.themeSwitch,
-            navigationBar: lightTheme.navigationBar,
-            backgroundColor: lightTheme.topHeaderPane.backgroundColor,
+          {
+            htmlID: "open-source-portfolio-section",
+            barLabel: "Open Source Portfolio",
           },
-          verticalSidePane: {
-            personalInfo: {
-              // pass Theme Personal Color Design
-              ...lightTheme.personal,
-              // adjust interface
-              linkColor: lightTheme.personal.urlTextColor,
-            },
-            education: {
-              item: lightTheme.education.item,
-            },
-          },
+        ]}
+        data={{
           verticalMainPane: {
-            introduction: lightTheme.introduction,
-            professional: lightTheme.professional,
-            portfolio: {
-              ...lightTheme.portfolio,
-              item: {
-                ...lightTheme.portfolio.item,
-                theme: {
-                  links: {
-                    ...lightTheme.portfolio.item.resourceLinks
+            introduction: {
+              name: personal.name,
+            },
+            professional: professional.experience_items,
+            portfolio: portfolio.map((item: any) => ({
+              ...item,
+              urlText: item.url,
+            })),
+
+
+            // ...portfolio,
+            // release: portfolio.release.map((item: any) => ({
+            //   ...item,
+            // }))
+            // },
+          },
+
+          verticalSidePane: {
+            personal: {
+              name: personal.name,
+              email: personal.email,
+              github: name2Url["github"],
+              gitlab: name2Url["gitlab"],
+              linkedin: name2Url["linkedin"],
+            },
+            education: education.map(
+              (item: EducationItemUserTextData, index: number) => ({
+                degree_title: item.degree,
+                university_name: item.name,
+                location: item.location,
+                duration: item.date,
+                thesis_title: item.thesis_title,
+                topics: item.topics,
+              })
+            ),
+          },
+        }}
+        colorSet={{
+          // LIGHT THEME - MODE
+          light: {
+            containerBackgroundColor: lightTheme.backgroundColor,
+            topHeaderPane: {
+              themeSwitch: lightTheme.themeSwitch,
+              navigationBar: lightTheme.navigationBar,
+              backgroundColor: lightTheme.topHeaderPane.backgroundColor,
+            },
+            verticalSidePane: {
+              personalInfo: {
+                // pass Theme Personal Color Design
+                ...lightTheme.personal,
+                // adjust interface
+                linkColor: lightTheme.personal.urlTextColor,
+              },
+              education: {
+                item: lightTheme.education.item,
+              },
+            },
+            verticalMainPane: {
+              introduction: lightTheme.introduction,
+              professional: lightTheme.professional,
+              portfolio: {
+                ...lightTheme.portfolio,
+                item: {
+                  ...lightTheme.portfolio.item,
+                  theme: {
+                    links: {
+                      ...lightTheme.portfolio.item.resourceLinks,
+                      item: {
+                        ...lightTheme.portfolio.item.resourceLinks.item,
+                        icons: Array.from({ length: maxNumberOfLinksPerPortfolioItems }, () => ({
+                          svgStyles: {
+                            width: "12px",
+                            height: "12px",
+                            fill: lightTheme.portfolio.item.resourceLinks.item.color,
+                          },
+                        })),
+                      },
+                    },
+                    releases: {
+                      ...lightTheme.portfolio.item.releases,
+                      headerFontFamily: lightTheme.portfolio.item.releases.fontFamily,
+                      headerColor: lightTheme.portfolio.item.releases.color,
+                      releaseButtonTheme: {
+                        ...lightTheme.portfolio.item.releases.item,
+                        icons: Array.from({ length: maxNumberOfReleasesPerPortfolioItems }, () => ({
+                          svgStyles: {
+                            width: "12px",
+                            height: "12px",
+                            fill: lightTheme.portfolio.item.releases.item.color,
+                          },
+                        })),
+                      },
+                    },
                   },
-                  releases: {
-                    headerFontFamily: lightTheme.portfolio.item.releases.fontFamily,
-                    headerColor: lightTheme.portfolio.item.releases.color,
-                    headerMarginBottom: lightTheme.portfolio.item.releases.headerMarginBottom,
-                    releaseButtonTheme: lightTheme.portfolio.item.releases.item
+                },
+              },
+              // ...lightTheme,
+              // containerBackgroundColor: lightTheme.backgroundColor,
+            },
+            bottomFooterPane: {
+              ...lightTheme.footerStyles,
+              // svgStyles: {
+              //   width: "15px",
+              //   height: "15px",
+              // }
+            },
+          },
+          // DARK THEME - MODE
+          dark: {
+            containerBackgroundColor: darkTheme.backgroundColor,
+            topHeaderPane: {
+              themeSwitch: darkTheme.themeSwitch,
+              navigationBar: darkTheme.navigationBar,
+              backgroundColor: darkTheme.topHeaderPane.backgroundColor,
+            },
+            verticalSidePane: {
+              personalInfo: {
+                // pass Theme Personal Color Design
+                ...darkTheme.personal,
+                // adjust interface
+                linkColor: darkTheme.personal.urlTextColor,
+              },
+              education: {
+                item: darkTheme.education.item,
+              },
+            },
+            verticalMainPane: {
+              ...darkTheme,
+              portfolio: {
+                ...darkTheme.portfolio,
+                item: {
+                  ...darkTheme.portfolio.item,
+                  theme: {
+                    links: {
+                      ...darkTheme.portfolio.item.resourceLinks,
+                      item: {
+                        ...darkTheme.portfolio.item.resourceLinks.item,
+                        icons: Array.from({ length: maxNumberOfLinksPerPortfolioItems }, () => ({
+                          svgStyles: {
+                            width: "12px",
+                            height: "12px",
+                            fill: darkTheme.portfolio.item.resourceLinks.item.color,
+                          },
+                        })),
+                      },
+                    },
+                    releases: {
+                      ...darkTheme.portfolio.item.releases,
+                      headerFontFamily: darkTheme.portfolio.item.releases.fontFamily,
+                      headerColor: darkTheme.portfolio.item.releases.color,
+                      releaseButtonTheme: {
+                        ...darkTheme.portfolio.item.releases.item,
+                        icons: Array.from({ length: maxNumberOfReleasesPerPortfolioItems }, () => ({
+                          svgStyles: {
+                            width: "12px",
+                            height: "12px",
+                            fill: darkTheme.portfolio.item.releases.item.color,
+                          },
+                        })),
+                      },
+                    },
                   },
                 },
               },
             },
-            // ...lightTheme,
-            // containerBackgroundColor: lightTheme.backgroundColor,
+            bottomFooterPane: darkTheme.footerStyles,
           },
-          bottomFooterPane: lightTheme.footerStyles,
-        },
-        // DARK THEME - MODE
-        dark: {
-          containerBackgroundColor: darkTheme.backgroundColor,
-          topHeaderPane: {
-            themeSwitch: darkTheme.themeSwitch,
-            navigationBar: darkTheme.navigationBar,
-            backgroundColor: darkTheme.topHeaderPane.backgroundColor,
-          },
-          verticalSidePane: {
-            personalInfo: {
-              // pass Theme Personal Color Design
-              ...darkTheme.personal,
-              // adjust interface
-              linkColor: darkTheme.personal.urlTextColor,
-            },
-            education: {
-              item: darkTheme.education.item,
-            },
-          },
-          verticalMainPane: {
-            ...darkTheme,
-            portfolio: {
-              ...darkTheme.portfolio,
-              item: {
-                ...darkTheme.portfolio.item,
-                theme: {
-                  links: {
-                    ...darkTheme.portfolio.item.resourceLinks
-                  },
-                  releases: {
-                    headerFontFamily: darkTheme.portfolio.item.releases.fontFamily,
-                    headerColor: darkTheme.portfolio.item.releases.color,
-                    headerMarginBottom: darkTheme.portfolio.item.releases.headerMarginBottom,
-                    releaseButtonTheme: darkTheme.portfolio.item.releases.item
-                  },
-                },
-              },
-            },
-
-          },
-          bottomFooterPane: darkTheme.footerStyles,
-        },
-      }}
-    />
+        }}
+      />
     // </main>
   );
 };

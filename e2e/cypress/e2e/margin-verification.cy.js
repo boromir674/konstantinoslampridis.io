ef0336467ab21d740acd8b19187eccd024ba94c2
konstantinos <boromir674@hotmail.com>
2024-07-04 02:14:22 +0300

    e2e: migrate to cypress 12 from 3 !

--- e2e/cypress/e2e/margin-verification.cy.js
+++ e2e/cypress/e2e/margin-verification.cy.js
@@ -1,5 +1,4 @@
-context("App", () => {
-  // Visit a web page.
+describe("template spec", () => {
   beforeEach(() => {
     cy.visit("/");
   });
@@ -7,11 +6,7 @@ context("App", () => {
   // ASSERT margin CSS property is not present
   // factoring browsers default agent styles, we expect a margin of 8px
   // assuming we haven't applied CSS reset
-
   it(`Verify margin CSS property is 8px, since no CSS reset`, () => {
-
-    cy.get('body').should('have.css', 'margin', '8px');
-
+    cy.get("body").should("have.css", "margin", "8px");
   });
-
 });

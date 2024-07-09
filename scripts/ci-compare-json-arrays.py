5a390c29784fa925b2fbd0f656805a74d2a82e11
konstantinos <boromir674@hotmail.com>
2024-07-09 20:50:42 +0300

    ci: allow runtime failed assertions to be less than gold-standard

--- scripts/ci-compare-json-arrays.py
+++ scripts/ci-compare-json-arrays.py
@@ -3,26 +3,40 @@
 import sys
 import json
 
-f1 = sys.argv[1]
-f2 = sys.argv[2]
+runtime_data_file = sys.argv[1]
+gold_standard_file = sys.argv[2]
 
-array1 = json.load(open(f1))
-array2 = json.load(open(f2))
+exit_code = 0
 
-# print(type(array1), type(array2))
-# print('\n', array1, '\n')
-# print(array1[0], '\n')
-# print(array2)
+runtime = json.load(open(runtime_data_file))
+gold_standard = json.load(open(gold_standard_file))
 
-# transform arrays/lists to dicts
-dict1 = {x['auditId']: x for x in array1}
-dict2 = {x['auditId']: x for x in array2}
+# transform runtime and Gold Standard (GS) lists to dicts
+data_runtime = {x['auditId']: x for x in runtime}
+data_gold_standard = {x['auditId']: x for x in gold_standard}
 
-assert len(array1) == len(array2), f"Length of arrays are different: {len(array1)} != {len(array2)}. Keys: {sorted(dict1.keys())} != {sorted(dict2.keys())}"
+# Failed Audit assertions to be at most the number of GS Fails: level errors + warn
+if len(runtime) > len(gold_standard):
+    print(f"[WARN] Audit assertions are more than the Gold Standard: {len(runtime)} <= {len(gold_standard)}")
+
+    # pretty print 2 columns of runtime and gs audidtId's
+    for i, (audit_id, k2) in enumerate(zip(sorted(data_runtime.keys()), sorted(data_gold_standard.keys()))):
+        print(f"{audit_id} {k2}")
+
+    # print rest of auditId's
+    for k in sorted(data_runtime.keys())[len(data_gold_standard):]:
+        print(f"{k} ")
+
+    sys.exit(exit_code)
+
+if len(runtime) < len(gold_standard):  # potential improvement over tracked GS
+    # can signal for raising standards by updating the tracked GS
+    print(f"[INFO] Audit assertions are less than the Gold Standard: {len(runtime)} <= {len(gold_standard)}")
+    print(f"[INFO] Gold Standard could be improved by removing some of the audit assertions it contains")
 
 
 # Compare dict1 and dict2
-assert set(dict1.keys()) == set(dict2.keys()), f"Keys are different between dict1 and dict2: {sorted(dict1.keys())} != {sorted(dict2.keys())}"
+# assert set(data_runtime.keys()) == set(data_gold_standard.keys()), f"Keys are different between dict1 and dict2: {sorted(data_runtime.keys())} != {sorted(data_gold_standard.keys())}"
 
 # Comparable attributes
 comparable = (
@@ -39,11 +53,17 @@ comparable = (
     'auditDocumentationLink',
 )
 
-for k1, v1 in dict1.items():
+for audit_id, v1 in data_runtime.items():
     for k in comparable:
-        assert v1[k] == dict2[k1][k], f"Values are different for key {k1} and attribute {k}: {v1[k]} != {dict2[k1][k]}"
-    
-    if v1['actual'] != dict2[k1]['actual']:
-        print(f"[WARN] Values are different for key {k1} and attribute actual: {v1['actual']} != {dict2[k1]['actual']}")
-    if v1['values'] != dict2[k1]['values']:
-        print(f"[WARN] Values are different for key {k1} and attribute values: {v1['values']} != {dict2[k1]['values']}")
+        assert v1[k] == data_gold_standard[audit_id][k], (
+            f"Audit '{audit_id}' attribute '{k}': 'Runtime' and Gold Standard' missmatch!\n"
+            "Might happen if new Lighthouse version is used in CI."
+            "Might happen if the Gold Standard was poorly updated.\n"
+            " In both cases, please regenerate the Gold Standard lighthouse assertion results and commit.\n"
+        )
+
+    if v1['actual'] != data_gold_standard[audit_id]['actual']:
+        print(f"[WARN] Values are different for key {audit_id} and attribute 'actual': {v1['actual']} != {data_gold_standard[audit_id]['actual']}")
+
+    if v1['values'] != data_gold_standard[audit_id]['values']:
+        print(f"[WARN] Values are different for key {audit_id} and attribute 'values': {v1['values']} != {data_gold_standard[audit_id]['values']}")

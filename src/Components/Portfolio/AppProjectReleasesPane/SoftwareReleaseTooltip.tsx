e54b56dd42370a95ab07f8628b805bd2031ec9a2
Konstantinos Lampridis <boromir674@hotmail.com>
2024-12-21 00:31:19 +0200

    feat: simple Copied pop-up on shell cmd user copy

--- src/Components/Portfolio/AppProjectReleasesPane/SoftwareReleaseTooltip.tsx
+++ src/Components/Portfolio/AppProjectReleasesPane/SoftwareReleaseTooltip.tsx
@@ -40,7 +40,6 @@ const MyLink = styled(Link)`
 // declared using forwardRef. This opts it into receiving the ref from above as the second ref argument which is declared after props.
 const SoftwareReleaseTooltip = forwardRef<HTMLDivElement, SoftwareReleaseTooltipProps>(({ theme, data: { command, urlText }, visible, onCopyCommand, copied }, ref) => {
     if (!visible) return null;
-
     return (
         // CONTAINER
         <div
@@ -63,7 +62,97 @@ const SoftwareReleaseTooltip = forwardRef<HTMLDivElement, SoftwareReleaseTooltip
                         {command}
                     </code>
                 </pre>
-                {copied && <span style={{ color: 'green', marginLeft: '10px' }}>Copied!</span>}
+                {/* OLD DESIGN */}
+                {/* {copied && <span style={{ color: 'green', marginLeft: '10px' }}>Copied!</span>} */}
+
+                {/* OPT 1 */}
+                {/* {copied && (
+                    <div style={{
+                        position: 'absolute',
+                        top: '-30px', // Adjust as needed
+                        left: '50%',
+                        transform: 'translateX(-50%)',
+                        padding: '5px 10px',
+                        backgroundColor: 'yellow',
+                        border: '1px solid black',
+                        borderRadius: '10px',
+                        boxShadow: '2px 2px 5px rgba(0,0,0,0.3)',
+                        zIndex: 1,
+                        whiteSpace: 'nowrap',
+                        fontSize: '12px',
+                        fontWeight: 'bold',
+                      }}>
+                    Copied!
+                    </div>
+                )} */}
+
+                {/* OPT 2 */}
+                {/* {copied && (
+                    <div style={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '-150px', // Adjust as needed
+                        transform: 'translateY(-50%)',
+                        padding: '5px 10px',
+                        backgroundColor: 'yellow',
+                        border: '1px solid black',
+                        borderRadius: '10px',
+                        boxShadow: '2px 2px 5px rgba(0,0,0,0.3)',
+                        zIndex: 1,
+                        whiteSpace: 'nowrap',
+                        fontSize: '12px',
+                        fontWeight: 'bold',
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}>
+                        <span style={{ marginRight: '5px' }}>Copied!</span>
+                        <div style={{
+                            width: '0',
+                            height: '0',
+                            borderLeft: '10px solid yellow',
+                            borderTop: '5px solid transparent',
+                            borderBottom: '5px solid transparent',
+                            position: 'absolute',
+                            right: '-10px',
+                        }}></div>
+                    </div>
+                )} */}
+            
+                {/* OPT 3 */}
+                {copied && (
+                    <div style={{
+                        position: 'absolute',
+                        // Smaller values move higher
+                        top: '27%', // Adjusted to be a bit higher
+                        // Smaller values move to the right
+                        left: '-70px', // Adjusted to be closer to the right
+                        transform: 'translateY(-50%)',
+                        padding: '5px 10px',
+                        backgroundColor: 'yellow',
+                        border: '1px solid black',
+                        borderRadius: '10px',
+                        boxShadow: '2px 2px 5px rgba(0,0,0,0.3)',
+                        zIndex: 1,
+                        whiteSpace: 'nowrap',
+                        fontSize: '12px',
+                        fontWeight: 'bold',
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}>
+                        <span style={{ marginRight: '5px' }}>Copied!</span>
+                        <div style={{
+                            width: '0',
+                            height: '0',
+                            borderLeft: '10px solid yellow',
+                            borderTop: '5px solid transparent',
+                            borderBottom: '5px solid transparent',
+                            position: 'absolute',
+                            right: '-10px',
+                        }}></div>
+                    </div>
+                )}
+
+
             </div>
             {/* WEB PAGE URL */}
             <div style={{

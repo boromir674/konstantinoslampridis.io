a113a2941b5168d6cfadcb35c1509c644e777ade
Konstantinos Lampridis <boromir674@hotmail.com>
2025-01-29 18:47:28 +0200

    add interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,4 @@
+import type { LayoutInterface, LayoutsObject } from './GridLayoutInterfaces';
+import IconStyles from './SVGIconStyles';
+
+export type { IconStyles, LayoutInterface, LayoutsObject };
